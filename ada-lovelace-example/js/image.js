ebde291794722031ae9b5f5d19355bbe2ecfde37
Tara78 <70331037+Tara78@users.noreply.github.com>
2020-12-19 12:07:28 +0100

    react

--- ada-lovelace-example/js/image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Image() {
-    return (
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg"></img>
-    )
-}
-
-export default Image;
--- ada-lovelace-example/js/index.js
+++ ada-lovelace-example/js/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-import Image from './image';
-import About from './about';
-
-function App() {
-    return (
-        <section>
-            <h1>Ada Lovelace</h1>
-            <Image />
-            <About />
-        </section>
-    )
-}
+import App from './App'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
